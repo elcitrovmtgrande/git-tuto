5917e19b7e89e5389d4004e466139d786ec3b22c
Victor MAHE <victormahe@ODMAC01598.local>
2023-05-12 14:17:55 +0200

    GP-1 | Créer un input permettant de saisir un nom et recuperer ce code dans une variable

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 document.addEventListener('DOMContentLoaded', function() {
 
+  let inputVal = '';
+
   const Button = document.getElementById('btn');
+  const Input = document.getElementById('input');
+
+  Input.addEventListener('keydown', function(e) {
+    inputVal = e.target.value;
+  });
   
 });
